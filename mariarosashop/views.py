3a0293b64ec0218f15b19152d04a93aab3221686
Gabriel Araujo <gabrielsant95@gmail.com>
2024-07-25 00:11:06 -0300

    Creating dinamic names

--- mariarosashop/views.py
+++ mariarosashop/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'mariarosashop/index.html')
+
+    dados = {
+        1: {"nome": "Nebulosa de Carina",
+            "legenda": "webtelescope.org / NASA /James Webb"},
+        2: {"nome": "Galaxia NGC 1079",
+            "legenda": "nada.org / NASA / Hubble"}
+    }
+
+    return render(request, 'mariarosashop/index.html', {"cards": dados})
 
 
 def imagem(request):
