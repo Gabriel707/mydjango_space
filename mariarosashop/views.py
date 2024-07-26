f251ca0970b9be4cd919ea25b87a8c1090521240
Gabriel Araujo <gabrielsant95@gmail.com>
2024-07-26 18:28:26 -0300

    Multiple changes on image display and storage

--- mariarosashop/views.py
+++ mariarosashop/views.py
@@ -7,5 +7,6 @@ def index(request):
     return render(request, 'mariarosashop/index.html', {"cards": fotografias})
 
 
-def imagem(request):
-    return render(request, 'mariarosashop/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'mariarosashop/imagem.html', {"fotografia": fotografia})
